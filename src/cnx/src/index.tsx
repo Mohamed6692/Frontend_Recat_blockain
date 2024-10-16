a3005712d11517322dc300e0e39f09711dd5b43d
Mohamed <mohamedcoulibaly66201@gmail.com>
2024-10-16 14:24:24 +0100

    JSON 4

--- src/cnx/src/index.tsx
+++ src/cnx/src/index.tsx
@@ -1,32 +1,32 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import '@rainbow-me/rainbowkit/styles.css';
+// import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { WagmiProvider } from 'wagmi';
+// import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
-import App from './App';
-import { config } from './wagmi';
-import { CustomAvatar } from './App'; // Importe le composant Avatar personnalisé
+// import App from './App';
+// import { config } from './wagmi';
+// import { CustomAvatar } from './App'; // Importe le composant Avatar personnalisé
 
-console.log('Configuration:', config);
+// console.log('Configuration:', config);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
-root.render(
-  <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider avatar={CustomAvatar}> {/* Ajoute l'avatar personnalisé ici */}
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <WagmiProvider config={config}>
+//       <QueryClientProvider client={queryClient}>
+//         <RainbowKitProvider avatar={CustomAvatar}> {/* Ajoute l'avatar personnalisé ici */}
+//           <App />
+//         </RainbowKitProvider>
+//       </QueryClientProvider>
+//     </WagmiProvider>
+//   </React.StrictMode>
+// );
--- src/cnx/src/wagmi.ts
+++ src/cnx/src/wagmi.ts
@@ -1,26 +1,26 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-  telos,
-} from 'wagmi/chains';
+// import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+// import {
+//   arbitrum,
+//   base,
+//   mainnet,
+//   optimism,
+//   polygon,
+//   sepolia,
+//   telos,
+// } from 'wagmi/chains';
 
-export const config = getDefaultConfig({
-  appName: 'RainbowKit demo',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    telos,
-    ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
-});
+// export const config = getDefaultConfig({
+//   appName: 'RainbowKit demo',
+//   projectId: 'YOUR_PROJECT_ID',
+//   chains: [
+//     mainnet,
+//     polygon,
+//     optimism,
+//     arbitrum,
+//     base,
+//     telos,
+//     ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+//   ],
+// });
 
-console.log('Configuration:', config);
+// console.log('Configuration:', config);
